6ba6e447a297222b7b452e91d2353f2954a436d2
Keisuke KITO <97872494+kitokjp@users.noreply.github.com>
2023-08-15 12:21:31 +0900

    Create __init__.py

---
+++ cats/server/__init__.py
@@ -0,0 +1,22 @@
+#  Copyright (C) 2023 Mitsubishi Electric Corporation.
+#
+# Licensed according to the license at the following URL
+#
+# https://github.com/catsploit/catsploit/LICENSE
+#
+# You may not use this file except in compliance with the License.
+#
+from .attack_platform import *
+from .attack_step import *
+from .attack_db import *
+from .component import *
+from .config import *
+from .constants import *
+from .dbms import *
+from .knowledgebase import *
+from .scenario_maker import *
+from .server import *
+from .session import *
+from .system import *
+from .task import *
+
