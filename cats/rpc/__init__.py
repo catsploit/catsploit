c1cba32e160569d0db9e7b1755d150713d134431
catsploit <138558020+catsploit@users.noreply.github.com>
2023-08-15 11:10:13 +0900

    Create __init__.py

---
+++ cats/rpc/__init__.py
@@ -0,0 +1,10 @@
+#  Copyright (C) 2023 Mitsubishi Electric Corporation.
+#
+# Licensed according to the license at the following URL
+#
+# https://github.com/catsploit/catsploit/LICENSE
+#
+# You may not use this file except in compliance with the License.
+#
+from .rpc import *
+from .connection import *
